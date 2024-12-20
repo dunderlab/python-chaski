56b4259fb8172b5984c67469e2d530f6e647288e
Yeison Cardona <yencardonaal@unal.edu.co>
2024-12-20 18:25:58 -0500

    Added ENV variables

--- chaski/__init__.py
+++ chaski/__init__.py
@@ -1,1 +1,12 @@
- 
+import os
+
+env = {
+    'STREAMER_ROOT': '127.0.0.1:65433',
+    'CERTIFICATE_AUTHORITY': '127.0.0.1:65432',
+    'REMOTE_PROXY': '127.0.0.1:65434',
+    'CELERY_BACKEND': 'chaski://127.0.0.1:65433',
+    'CELERY_BROKER': 'chaski://127.0.0.1:65433',
+}
+
+for key in env:
+    os.environ[f'CHASKI_{key}'] = env[key]
